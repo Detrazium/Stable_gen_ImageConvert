7150549310b0c8235cd5a28e310b040195e853d7
Detrazium <lazutckin.anton@yanfex.ru>
2024-05-29 23:30:45 +0300

    commit #7.2

--- diffusion_stable.py
+++ diffusion_stable.py
@@ -6,25 +6,30 @@ from diffusers import (
 ControlNetModel,
 StableDiffusionControlNetImg2ImgPipeline,
 DDIMScheduler,)
-from diffusers.utils import load_image, make_image_grid
+from diffusers.utils import make_image_grid, load_image
 import torch
 import numpy as np
 
-
-
 class Stable_patterns_metod_v1():
-	def __init__(self, image=None):
+	def __init__(self):
 		self.modelLycon = "Lykon/DreamShaper"
 		self.contNet_depth = "lllyasviel/sd-controlnet-depth"
 		self.face_sd15Adapt = 'ip-adapter-plus-face_sd15.bin'
-		self.image = self.get_image(image)
-	def get_image(self, img):
-		if img == None:
-			raise ValueError('None image, please get image')
-		img = load_image(img)
-		return img
-	def create_image(self, prompt, bad_prompt):
-		imgMy = self.image
+		self.get_model()
+	def get_model(self):
+		controlnet = ControlNetModel.from_pretrained(self.contNet_depth)
+		pipline = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(self.modelLycon,
+																		   controlnet=controlnet)
+		pipline.scheduler = DDIMScheduler.from_config(pipline.scheduler.config)
+		pipline.load_ip_adapter("h94/IP-Adapter",
+								subfolder='models',
+								weight_name=self.face_sd15Adapt)
+		pipline.set_ip_adapter_scale(0.6)
+
+		self.pipline = pipline
+
+	def create_image(self, imager):
+		self.imgMy = imager
 		def get_map_depth(imgMy, depth_ester):
 			img = depth_ester(imgMy)['depth']
 			img = np.array(img)
@@ -34,20 +39,19 @@ class Stable_patterns_metod_v1():
 			depth_map = detecter.permute(2, 0, 1)
 			return depth_map
 		depth_ester = pipeline('depth-estimation')
-		depth_image = get_map_depth(self.image, depth_ester).unsqueeze(0).half()
-		controlnet = ControlNetModel.from_pretrained(self.contNet_depth)
-		pipline = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(self.modelLycon,controlnet=controlnet)
-		pipline.scheduler = DDIMScheduler.from_config(pipline.scheduler.config)
-		pipline.load_ip_adapter("h94/IP-Adapter", subfolder='models', weight_name=self.face_sd15Adapt)
-		pipline.set_ip_adapter_scale(0.6)
-		generatro = torch.Generator(device='cpu').manual_seed(33)
-		outer = pipline(prompt=prompt,
-						negative_prompt=bad_prompt,
-						image=imgMy,
-						ip_adapter_image=imgMy,
-						control_image = depth_image,
-						generator=generatro).images[0]
-		item = make_image_grid([imgMy, outer], rows=1, cols=2)
+		self.depth_image = get_map_depth(self.imgMy, depth_ester).unsqueeze(0).half()
+	def gen_start(self, images, prompt, bad_prompt):
+		self.create_image(images)
+
+		generatro = torch.Generator().manual_seed(33)
+		outer = self.pipline(
+			prompt=prompt,
+			negative_prompt=bad_prompt,
+			image=self.imgMy,
+			ip_adapter_image=self.imgMy,
+			control_image = self.depth_image,
+			generator=generatro).images[0]
+		item = make_image_grid([self.imgMy, outer], rows=1, cols=2)
 		return item
 
 def main():
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-image = 'imgs/imager.jpg'
-prompt = 'masterpiece, portrait of a person, anime style, high quality, RAW photo, 8k uhd'
-prompt2 = 'masterpiece, portrait of a person, anime style, high quality, RAW photo, 8k uhd, dragon, necronomicon, dragon man'
-bad_prompt = 'monochrome, lowres, bad anatomy, worst quality, low quality, blurry'
-
 from diffusion_stable import Stable_patterns_metod_v1
+from diffusers.utils import load_image
 
-img = Stable_patterns_metod_v1(image).create_image(prompt=prompt2, bad_prompt=bad_prompt)
-img.save('imgs/imager1.jpg')
+image = 'https://s565vla.storage.yandex.net/rdisk/75cd38644df97568bd0d17a7dfff0661f7d3b7ad25230f2e638cb7765e5de880/664cc783/z2Hg9rf1o6KeuJ7Nd_tI11Ucr2_IGd7VQopCEeOZheReHV5r3AaW2bzTJxdROH5vu-w9jIL9uEQ6UQ-U-o4Z4A==?uid=0&filename=Image_.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&fsize=191565&hid=7d1fdb13425cb90c9a42494da164a660&media_type=image&tknv=v2&etag=abd8ea2d21daebe70dd23d95055c5d6b&ts=618f90f4ea6c0&s=81cb85b11b1e31e2ea43d3259639bb9d887d2c8924a7d0d092830528374ae410&pb=U2FsdGVkX18y7sfDPHoLL10vxFpZeNT4Jy2D_gmjCoDwAiS-PETaoY7QRQFjgK7BdBrMNXALd5W75kp0KOavVzp609hlZXZhclaVP5GviSc'
+prompt = "masterpiece, portrait of a person, anime style, high quality, RAW photo, 8k uhd"
+bad_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality, blurry"
+def main():
+    Stable_patterns_metod_v1().gen_start(images=load_image(image), prompt=prompt, bad_prompt=bad_prompt)
+
+if __name__ == '__main__':
+    main()
