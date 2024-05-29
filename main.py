65b9c2aecc3c04b653a78cc78abae35cecd7723a
Detrazium <lazutckin.anton@yanfex.ru>
2024-05-29 23:49:32 +0300

    commit #7.4

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from diffusion_stable import Stable_patterns_metod_v1
 from diffusers.utils import load_image
 
-prompt = "masterpiece, portrait of a person, anime style, high quality, RAW photo, 8k uhd"
 bad_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality, blurry"
 def main():
     Stable_patterns_metod_v1().gen_start(images=load_image(image), prompt=prompt, bad_prompt=bad_prompt)
