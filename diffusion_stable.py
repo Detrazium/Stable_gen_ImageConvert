f6db80d7c4950bf0ccef448865e0c64de19f2a75
Detrazium <lazutckin.anton@yanfex.ru>
2024-05-29 23:54:05 +0300

    commit #8 finish

--- diffusion_stable.py
+++ diffusion_stable.py
@@ -1,62 +1,61 @@
 import os, logging
+
+import torch
+
 logging.disable(logging.WARNING)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from diffusers import StableDiffusionControlNetImg2ImgPipeline, ControlNetModel, UniPCMultistepScheduler
+from diffusers.utils import load_image
 from transformers import pipeline
-from diffusers import (
-ControlNetModel,
-StableDiffusionControlNetImg2ImgPipeline,
-DDIMScheduler,)
-from diffusers.utils import make_image_grid, load_image
-import torch
+from PIL import Image
 import numpy as np
 
-class Stable_patterns_metod_v1():
-	def __init__(self):
-		self.modelLycon = "Lykon/DreamShaper"
-		self.contNet_depth = "lllyasviel/sd-controlnet-depth"
-		self.face_sd15Adapt = 'ip-adapter-plus-face_sd15.bin'
-		self.get_model()
-	def get_model(self):
-		controlnet = ControlNetModel.from_pretrained(self.contNet_depth)
-		pipline = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(self.modelLycon,
-																		   controlnet=controlnet)
-		pipline.scheduler = DDIMScheduler.from_config(pipline.scheduler.config)
-		pipline.load_ip_adapter("h94/IP-Adapter",
-								subfolder='models',
-								weight_name=self.face_sd15Adapt)
-		pipline.set_ip_adapter_scale(0.6)
+class stt():
+    def __init__(self):
+        self.controlnetURL = "lllyasviel/sd-controlnet-depth"
+        self.ipadapter = 'ip-adapter-plus-face_sd15.bin'
+        self.modelLycons = "Lykon/DreamShaper"
+        self.loadd_model()
+    def loadd_model(self):
+        controlnet = ControlNetModel.from_pretrained(self.controlnetURL, torch_dtype=torch.float32)
+        pipline = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(self.modelLycons,
+                                                                            controlnet=controlnet,
+                                                                           torch_dtype=torch.float32).to('cpu')
+        pipline.enable_vae_tiling()
+        pipline.load_ip_adapter("h94/IP-Adapter",
+                                 subfolder='models',
+                                 weight_name=self.ipadapter)
+        pipline.set_ip_adapter_scale(0.6)
+        self.pipeline = pipline
+    def depth_map(self, img):
+        depthest = pipeline('depth-estimation')
 
-		self.pipline = pipline
+        img = depthest(img)['depth']
+        img = np.array(img)
+        img = img[:, :, None]
+        img = np.concatenate([img, img, img], axis=2)
+        depthmap = Image.fromarray(img)
+        return depthmap
 
-	def create_image(self, imager):
-		self.imgMy = imager
-		def get_map_depth(imgMy, depth_ester):
-			img = depth_ester(imgMy)['depth']
-			img = np.array(img)
-			img = img[:, :, None]
-			img = np.concatenate([img, img, img], axis=2)
-			detecter = torch.from_numpy(img).float() / 255.0
-			depth_map = detecter.permute(2, 0, 1)
-			return depth_map
-		depth_ester = pipeline('depth-estimation')
-		self.depth_image = get_map_depth(self.imgMy, depth_ester).unsqueeze(0).half()
-	def gen_start(self, images, prompt, bad_prompt):
-		self.create_image(images)
+    def start(self, image = None, prompt=None):
+        self.img = load_image(image)
+        self.control_image = self.depth_map(self.img)
 
-		generatro = torch.Generator().manual_seed(33)
-		outer = self.pipline(
-			prompt=prompt,
-			negative_prompt=bad_prompt,
-			image=self.imgMy,
-			ip_adapter_image=self.imgMy,
-			control_image = self.depth_image,
-			generator=generatro).images[0]
-		item = make_image_grid([self.imgMy, outer], rows=1, cols=2)
-		return item
+        generator = torch.manual_seed(303)
+        image = self.pipeline(
+            image=self.img,
+            prompt=prompt,
+            ip_adapter_image=self.img,
+            generator=generator,
+            control_image = self.control_image,
+            num_inference_steps=50
+        ).images[0]
+
+        image.save('n.png')
+        return image
 
 def main():
-	Stable_patterns_metod_v1()
-
+    stt()
 if __name__ == '__main__':
-	main()
+    main()
 
